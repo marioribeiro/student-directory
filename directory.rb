32fe55c1f7965da697d08d64a88683124e9036ec
Mário Ribeiro <mail@marioribeiro.com>
2017-04-25 21:48:37 +0100

    Add method to receive students from user

--- directory.rb
+++ directory.rb
@@ -1,18 +1,20 @@
-#Put all students into an array
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name os not empty, repeat this code
 
-students = [
-  { name: "Dr. Hannibal Lecter", cohort: :november },
-  { name: "Darth Vader", cohort: :november },
-  { name: "Nurse Ratched", cohort: :november },
-  { name: "Michael Corleone", cohort: :november },
-  { name: "Alex DeLarge", cohort: :november },
-  { name: "The Wicked Witch of the West", cohort: :november },
-  { name: "Terminator", cohort: :november },
-  { name: "Freddy Krueger", cohort: :november },
-  { name: "The Joker", cohort: :november },
-  { name: "Joffrey Baratheon", cohort: :november },
-  { name: "Norman Bates", cohort: :november }
-]
+  while !name.empty? do
+    students << { name: name, cohort: :november }
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
 
 def print_header
   puts "The students of Villains Academy"
@@ -27,8 +29,9 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
-#call the methods
+# call the methods
 
+students = input_students
 print_header
 print(students)
 print_footer(students)
